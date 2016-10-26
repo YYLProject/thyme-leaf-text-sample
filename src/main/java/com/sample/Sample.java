1142ce80b1f0887f4a2988eec9c9fe5f2e19f835
tomo taka <tomo_taka01@hotmail.com>
2016-10-26 11:00:47 +0900

    first commit

---
+++ src/main/java/com/sample/Sample.java
@@ -0,0 +1,36 @@
+package com.sample;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+public class Sample {
+    public static void main(String... args){
+        Sample sample = new Sample();
+        sample.createText();
+    }
+
+    void createText(){
+        Context context = new Context();
+        context.setVariable("targetId", "12345");
+
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setTemplateMode(TemplateMode.TEXT);
+        TemplateEngine engine = new TemplateEngine();
+        engine.addTemplateResolver(resolver);
+        Path path = Paths.get("sample.txt");
+
+        try (Writer writer = Files.newBufferedWriter(path)) {
+            engine.process("sample.template", context, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
