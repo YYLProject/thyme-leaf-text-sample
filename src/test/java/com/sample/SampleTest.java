1afd07d84c72e5aba3fb93f08a247a3f10cc9e1d
tomo taka <tomo_taka01@hotmail.com>
2016-10-26 17:06:20 +0900

    fix test

--- src/test/java/com/sample/SampleTest.java
+++ src/test/java/com/sample/SampleTest.java
@@ -1,13 +1,21 @@
 package com.sample;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
 
 public class SampleTest {
     @Test
     public void createSample() {
         Sample target = new Sample();
         target.createText();
-        assertEquals(1,1);
+        // check if sample.txt created
+        Path path = Paths.get("out", "sample.txt");
+        boolean result = Files.exists(path);
+        assertTrue("should be exists", result);
     }
 }
